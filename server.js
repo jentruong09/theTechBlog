1221f13a862bb22dd3e313a9b8104ed8b94d9ead
Jen Truong <jennifertruong09@gmail.com>
2022-03-01 17:10:36 -0500

    added handlebar pages

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ const PORT = process.env.PORT || 3001;
 
 // Set up sessions
 // const sess = {
-//   secret: 'Super secret secret',
-//   resave: false,
-//   saveUninitialized: true,
-// };
+//     secret: 'Super secret secret',
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize
+//     })
+//   };
 
 // app.use(session(sess));
 
