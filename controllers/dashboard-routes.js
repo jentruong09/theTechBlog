67362b505bce92229a6b3bdc7aa5586323968b0a
Jen Truong <jennifertruong09@gmail.com>
2022-03-02 20:34:30 -0500

    made changes to the dashboarf routes and handlebar, still try to make it work

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -26,7 +26,7 @@ router.get('/', withAuth, (req, res) => {
       
     }).then(dbPostData => {
         const posts = dbPostData.map((post) => post.get({ plain: true }));
-        res.render('homepage', {posts, loggedIn: req.session.loggedIn});
+        res.render('dashboard', {posts, loggedIn: req.session.loggedIn});
     })
     .catch(err => {
       console.log(err);
