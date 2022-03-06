bc87eeac3190a4f09004c39e4b1bd197fa0fd646
Jen Truong <jennifertruong09@gmail.com>
2022-03-06 13:11:27 -0500

    edit post was giving an error, fixed the error

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -91,26 +91,26 @@ router.post('/', withAuth, (req, res) => {
 });
 
 
-// router.put('/:id', withAuth, (req, res) => {
-//     Post.update({
-//         title: req.body.title,
-//         post: req.body.post
-//     },
-//     {
-//         where: {
-//             id: req.params.id
-//         }
-//     }).then (dbPostData => {
-//         if(!dbPostData) {
-//             res.status(404).json({ message: 'No post found with this id!' })
-//             return;
-//         } res.json(dbPostData)
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     })    
-// });
+router.put('/:id', withAuth, (req, res) => {
+    Post.update({
+        title: req.body.title,
+        post: req.body.post
+    },
+    {
+        where: {
+            id: req.params.id
+        }
+    }).then (dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id!' })
+            return;
+        } res.json(dbPostData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })    
+});
 
 router.post('/', withAuth, async (req, res) => {
     const body = req.body;
